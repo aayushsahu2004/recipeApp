a754ffa21aa04719d76fe428a56cdc4a9ecb63fb
aayushsahu2004 <sahuaayush2999@gmail.com>
2024-05-09 11:13:32 +0530

    Show Recipe Details and Delete Recipe

--- src/Router/RouterComponent.jsx
+++ src/Router/RouterComponent.jsx
@@ -4,17 +4,19 @@ import About from "../components/About"
 import Contact from "../components/Contact"
 import Recipe from "../components/Recipe"
 import Create from "../components/Create"
+import Details from "../components/Details"
 
 const RouterComponent = () => {
   return (
     <div>
-        <Routes>
-            <Route path="/" element={<Layout/>}/>
-            <Route path="/recipe" element={<Recipe/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/create-recipe" element={<Create/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/create-recipe" element={<Create />} />
+        <Route path="/recipe/:id" element={<Details />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,9 +2,12 @@ import { nanoid } from 'nanoid';
 import React, { useContext, useState } from 'react'
 import { toast } from 'react-toastify';
 import { Recipecontext } from '../context/RecipeContext';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
 
+    const nevigator = useNavigate();
+    
     const [recipe, setrecipe] = useContext(Recipecontext);
 
     const [image, setimage] = useState('');
@@ -22,7 +25,7 @@ const Create = () => {
             discription,
             ingredients,
             instructions
-        }
+        };
 
         setrecipe([...recipe, newRecipe]);
 
@@ -31,7 +34,8 @@ const Create = () => {
             "recipe",
             JSON.stringify([...recipe, newRecipe])
         );
-        toast.success("Recipe Created Successfully!")
+        toast.success("Recipe Created Successfully!");
+        nevigator("/recipe");
     };
 
 
@@ -42,7 +46,7 @@ const Create = () => {
                 <form onSubmit={SubmitHandler}>
                     <input onChange={(e) => setimage(e.target.value)} value={image} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6' type="url" placeholder='Recipe Image URL' />
                     <input onChange={(e) => setrecipeName(e.target.value)} value={recipeName} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6' type="text" placeholder='Recipe Name' />
-                    <textarea onChange={(e) => setdiscription(e.target.value)} value={discription} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6 resize-none' placeholder='Recipe Discription'></textarea>
+                    <textarea onChange={(e) => setdiscription(e.target.value)} value={discription} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6 resize-none' placeholder='Recipe Description'></textarea>
                     <textarea onChange={(e) => setingredients(e.target.value)} value={ingredients} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6 resize-none' placeholder="Recipe Ingredients -> 'use comma to seperate ingredients'..."></textarea>
                     <textarea onChange={(e) => setinstructions(e.target.value)} value={instructions} className='w-full py-3 px-6 border border-zinc-400 rounded-md mb-6 resize-none' placeholder="Recipe Instructions -> 'use comma to seperate instructions'..."></textarea>
                     <div className='w-full text-right'>
